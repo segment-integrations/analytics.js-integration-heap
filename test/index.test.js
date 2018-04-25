d96fbd37ee78ec76bf7abdf71e318965d53eec16
james727 <jameshkatz@gmail.com>
2018-04-25 10:33:05 -0400

    Send segment user id field on basic identify calls

--- test/index.test.js
+++ test/index.test.js
@@ -98,14 +98,14 @@ describe('Heap', function() {
         analytics.called(window.heap.addUserProperties, { trait: true, _email: 'email@email.org' });
       });
 
-      it('should send id as handle', function() {
+      it('should send id as handle with the correct field name', function() {
         analytics.identify('id');
-        analytics.called(window.heap.identify, 'id');
+        analytics.called(window.heap.identify, 'id', 'Segment User Id');
       });
 
       it('should send id as handle and traits', function() {
         analytics.identify('id', { trait: 'trait' });
-        analytics.called(window.heap.identify, 'id');
+        analytics.called(window.heap.identify, 'id', 'Segment User Id');
         analytics.called(window.heap.addUserProperties, { id: 'id', trait: 'trait' });
       });
 
@@ -124,7 +124,7 @@ describe('Heap', function() {
           { B: 'Peanut', C: true }
           ]
         });
-        analytics.called(window.heap.identify, 'id');
+        analytics.called(window.heap.identify, 'id', 'Segment User Id');
         analytics.called(window.heap.addUserProperties, {
           id: 'id',
           _email: 'teemo@teemo.com',
@@ -138,7 +138,7 @@ describe('Heap', function() {
       it('should send date traits as ISOStrings', function() {
         var date = new Date('2016');
         analytics.identify('id', { date: date });
-        analytics.called(window.heap.identify, 'id');
+        analytics.called(window.heap.identify, 'id', 'Segment User Id');
         analytics.called(window.heap.addUserProperties, { id: 'id', date: '2016-01-01T00:00:00.000Z' });
       });
     });
